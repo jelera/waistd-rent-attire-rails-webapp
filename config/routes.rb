1aab904da21e0110db43f8c8f012b97ef7eb32ca
github username <github email address>
2019-05-06 17:40:48 -0500

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cart_products
+  resources :outfit_products
+  resources :outfits
+  resources :products
+  resources :carts
+  resources :users
+
 end
--- db/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :cart_products
-  resources :outfit_products
-  resources :outfits
-  resources :products
-  resources :carts
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
