66946197d966bc8d241ff1b92355a380a934030b
Sarah Elzinga <sevans924@gmail.com>
2019-05-07 16:51:14 -0500

    added men's accessories

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ m_shoe1 = Product.create(
   description: "Lace-up oxford dress shoe"
   color: "BROWN"
   preferred_style: "Formal"
-  product_type: "Shoe"
+  product_type: "Shoes"
   price: 10
   outfit_gender: M
   brand: "ALLEN EDMONDS"
@@ -26,7 +26,7 @@ m_shoe2 = Product.create(
   description: "Lace-up oxford dress shoe"
   color: "BLACK"
   preferred_style: "Formal"
-  product_type: "Shoe"
+  product_type: "Shoes"
   price: 10
   outfit_gender: "M"
   brand: "ALLEN EDMONDS"
@@ -40,7 +40,7 @@ m_shoe3 = Product.create(
   description: "A mid-high ankle boot crafted from smooth leather with tonal side goring and a cushioned rubber sole exudes distinguished mod style on the daily."
   color: "BROWN"
   preferred_style: "Casual"
-  product_type: "Shoe"
+  product_type: "Shoes"
   price: 10
   outfit_gender: "M"
   brand: "NORDSTROM"
@@ -54,7 +54,7 @@ m_shoe4 = Product.create(
   description: "A mid-high ankle boot crafted from smooth leather with tonal side goring and a cushioned rubber sole exudes distinguished mod style on the daily."
   color: "BLACK"
   preferred_style: "Casual"
-  product_type: "Shoe"
+  product_type: "Shoes"
   price: 10
   outfit_gender: "M"
   brand: "NORDSTROM"
@@ -68,7 +68,7 @@ m_shoe5 = Product.create(
   description: "This low boot has a sleek, sharp profile that compliments dress looks and elevates jeans."
   color: "COLA SUEDE"
   preferred_style: "Casual"
-  product_type: "Shoe"
+  product_type: "Shoes"
   price: 10
   outfit_gender: "M"
   brand: "CLARKS"
@@ -82,7 +82,7 @@ w_shoe1 = Product.create(
   description: "Supple leather graces the silhouette of a poised pointy-toe pump lifted perfectly by a slightly slanted heel."
   color: "BLACK"
   preferred_style: "Formal"
-  product_type: "Shoe"
+  product_type: "Shoes"
   price: 10
   outfit_gender: "W"
   brand: "VINCE CAMUTO"
@@ -96,7 +96,7 @@ w_shoe2 = Product.create(
   description: "Supple leather graces the silhouette of a poised pointy-toe pump lifted perfectly by a slightly slanted heel."
   color: "SOFT MAUVE"
   preferred_style: "Formal"
-  product_type: "Shoe"
+  product_type: "Shoes"
   price: 10
   outfit_gender: "W"
   brand: "VINCE CAMUTO"
@@ -110,7 +110,7 @@ w_shoe3 = Product.create(
   description: "A cleanly styled flat features an on-trend pointed toe and a soft, cushioned footbed."
   color: "BLUSH SUEDE"
   preferred_style: "Casual"
-  product_type: "Shoe"
+  product_type: "Shoes"
   price: 10
   outfit_gender: "W"
   brand: "BELLA VITA"
@@ -124,7 +124,7 @@ w_shoe4 = Product.create(
   description: "A cleanly styled flat features an on-trend pointed toe and a soft, cushioned footbed."
   color: "BLACK SUEDE"
   preferred_style: "Casual"
-  product_type: "Shoe"
+  product_type: "Shoes"
   price: 10
   outfit_gender: "W"
   brand: "BELLA VITA"
@@ -134,6 +134,147 @@ w_shoe4 = Product.create(
 )
 
 w_shoe5 = Product.create(
+  name: "POPPY SANDALS"
+  description: "A sling-back sandal in leather with a stacked heel."
+  color: "BLACK"
+  preferred_style: "Casual"
+  product_type: "Shoes"
+  price: 10
+  outfit_gender: "W"
+  brand: "TOMS"
+  season: "Spring"
+  picture_url: "https://media03.toms.com/static/www/images/Womens-Shoes/2018/FEBLAUNCH/10011698-BlackLeatherWomensPoppy-P-1450x1015.jpg"
+  rating: 5
+)
+
+
+m_acc1 = Product.create(
+  name: "SILVER BUCKLE LEATHER DRESS BELT"
+  description: "A sling-back sandal in leather with a stacked heel."
+  color: "BLACK"
+  preferred_style: "Formal"
+  product_type: "Accessories"
+  price: 5
+  outfit_gender: "M"
+  brand: "BROOKS BROTHERS"
+  season: "All"
+  picture_url: "https://ashleyweston.com/wp-content/uploads/2017/03/Black-and-Brown-Dress-Belt-Ashley-Weston-e1489992565501.png"
+  rating: 5
+)
+
+m_acc2 = Product.create(
+  name: "SILVER BUCKLE LEATHER DRESS BELT"
+  description: "A sling-back sandal in leather with a stacked heel."
+  color: "BROWN"
+  preferred_style: "Formal"
+  product_type: "Accessories"
+  price: 5
+  outfit_gender: "M"
+  brand: "BROOKS BROTHERS"
+  season: "All"
+  picture_url: "https://ashleyweston.com/wp-content/uploads/2017/03/Black-and-Brown-Dress-Belt-Ashley-Weston-e1489992565501.png"
+  rating: 5
+)
+
+m_acc3 = Product.create(
+  name: "PEBBLE LEATHER SLIM BRIEFCASE"
+  description: "An elevated option for work, our soft pebble leather briefcase features twill lining, a large document pocket and small interior pockets. Shiny silver hardware."
+  color: "BROWN"
+  preferred_style: "Formal"
+  product_type: "Accessories"
+  price: 10
+  outfit_gender: "M"
+  brand: "BROOKS BROTHERS"
+  season: "All"
+  picture_url: "https://brooksbrothers.scene7.com/is/image/BrooksBrothers/UL00175_BROWN?$bbthumbnails2$"
+  rating: 5
+)
+
+m_acc4 = Product.create(
+  name: "RONDE SOLO DE CARTIER WATCH"
+  description: "Ronde Solo de Cartier watch, 42 mm, mechanical movement with automatic winding. Steel case, beaded crown set with a synthetic cabochon-shaped spinel, silvered opaline dial, blued-steel sword-shaped hands, sapphire crystal, black grained calfskin interchangeable strap, steel double adjustable folding buckle, calendar aperture at 3 o'clock."
+  color: "BLACK"
+  preferred_style: "Formal"
+  product_type: "Accessories"
+  price: 10
+  outfit_gender: "M"
+  brand: "CARTIER"
+  season: "All"
+  picture_url: "https://www.cartier.com/content/dam/rcq/car/15/15/35/9/1515359.png.scale.314.high.ronde-solo-de-cartier-watch-steel.jpg"
+  rating: 5
+)
+
+m_acc5 = Product.create(
+  name: "SISTEM CASUAL"
+  description: "The name says it all: stainless steel SISTEM CASUAL takes it easy with a relaxed look. The caseback of this mechanical Swatch features a white and grey checkered pattern."
+  color: "SILVER"
+  preferred_style: "Casual"
+  product_type: "Accessories"
+  price: 5
+  outfit_gender: "M"
+  brand: "SWATCH"
+  season: "All"
+  picture_url: "https://static.swatch.com/images/product/YIS421G/sa000/YIS421G_sa000_nvz.jpg"
+  rating: 5
+)
+
+w_acc1 = Product.create(
+  name: "POPPY SANDALS"
+  description: "A sling-back sandal in leather with a stacked heel."
+  color: "BLACK"
+  preferred_style: "Casual"
+  product_type: "Shoe"
+  price: 10
+  outfit_gender: "W"
+  brand: "TOMS"
+  season: "Spring"
+  picture_url: "https://media03.toms.com/static/www/images/Womens-Shoes/2018/FEBLAUNCH/10011698-BlackLeatherWomensPoppy-P-1450x1015.jpg"
+  rating: 5
+)
+
+w_acc2 = Product.create(
+  name: "POPPY SANDALS"
+  description: "A sling-back sandal in leather with a stacked heel."
+  color: "BLACK"
+  preferred_style: "Casual"
+  product_type: "Shoe"
+  price: 10
+  outfit_gender: "W"
+  brand: "TOMS"
+  season: "Spring"
+  picture_url: "https://media03.toms.com/static/www/images/Womens-Shoes/2018/FEBLAUNCH/10011698-BlackLeatherWomensPoppy-P-1450x1015.jpg"
+  rating: 5
+)
+
+w_acc3 = Product.create(
+  name: "POPPY SANDALS"
+  description: "A sling-back sandal in leather with a stacked heel."
+  color: "BLACK"
+  preferred_style: "Casual"
+  product_type: "Shoe"
+  price: 10
+  outfit_gender: "W"
+  brand: "TOMS"
+  season: "Spring"
+  picture_url: "https://media03.toms.com/static/www/images/Womens-Shoes/2018/FEBLAUNCH/10011698-BlackLeatherWomensPoppy-P-1450x1015.jpg"
+  rating: 5
+)
+
+w_acc4 = Product.create(
+  name: "POPPY SANDALS"
+  description: "A sling-back sandal in leather with a stacked heel."
+  color: "BLACK"
+  preferred_style: "Casual"
+  product_type: "Shoe"
+  price: 10
+  outfit_gender: "W"
+  brand: "TOMS"
+  season: "Spring"
+  picture_url: "https://media03.toms.com/static/www/images/Womens-Shoes/2018/FEBLAUNCH/10011698-BlackLeatherWomensPoppy-P-1450x1015.jpg"
+  rating: 5
+)
+
+w_acc5 = Product.create(
   name: "POPPY SANDALS"
   description: "A sling-back sandal in leather with a stacked heel."
   color: "BLACK"
