36a7dd0f55862e9ae0d900b869f472668a2a7b76
diautzi <dia_utzi@yahoo.com>
2019-05-08 11:04:44 -0500

    controller,views and routes for outfits

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,2 +1,29 @@
 class OutfitsController < ApplicationController
+
+  def index
+    @outfits = Outfit.all
+  end
+
+  def show
+    @outfit = Outfit.find(params[:id])
+    @product = @outfit.products
+  end
+
+
+  private
+
+  def outfits_params
+    params.require(:outfits).permit(:name, :description, :preferred_style, :picture_url, :outfit_gender, :rating)
+  end
+
 end
+
+# t.string "name"
+# t.string "description"
+# t.string "preferred_style"
+# t.string "picture_url"
+# t.string "outfit_gender"
+# t.integer "rating"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
+# end
