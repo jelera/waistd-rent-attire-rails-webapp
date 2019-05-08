b7c8c472a01e6844a7f3dcaf3855a9c7ceff1582
Sarah Elzinga <sevans924@gmail.com>
2019-05-08 10:21:00 -0500

    Added empty methods to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+
+
+
+def index
+end
+
+def show
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+   user = User.find(params[:id])
+   user.destroy
+   redirect_to root_path
+end
+
+private
+
+def user_params
+  params.require(:user).permit()
+end
+
+def find_user
+  @user = User.find(params[:id])
+end
+
 end
