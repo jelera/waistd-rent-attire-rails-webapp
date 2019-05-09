48de19266a304942b59116b9a7d5a689a4e00a25
Jose Elera <jelera@gmail.com>
2019-05-09 18:02:48 -0500

    Modify Outfit/controller

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,13 +1,11 @@
 class OutfitsController < ApplicationController
+  helper_method :add_to_my_outfits
+
 
   def index
     @outfits = Outfit.all
   end
 
-  def show
-    @outfit = Outfit.find(params[:id])
-    @products = @outfit.products
-  end
 
   def homepage
     if logged_in?
@@ -17,6 +15,35 @@ class OutfitsController < ApplicationController
     end
   end
 
+  def add_to_my_outfits
+    if logged_in?
+      user = User.find(session[:user_id])
+      outfit = Outfit.find(params[:id])
+
+      unless user.outfits.include? outfit
+        user.outfits << outfit
+        redirect_to outfit_path(outfit)
+      end
+
+    end
+  end
+
+  def remove_from_my_outfits
+    if logged_in?
+      user = User.find(session[:user_id])
+      outfit = Outfit.find(params[:id])
+
+      if user.outfits.include? outfit
+        user.outfits.delete(outfit)
+        redirect_to outfit_path(outfit)
+      end
+    end
+  end
+
+  def show
+    @outfit = Outfit.find(params[:id])
+    @products = @outfit.products
+  end
 
   private
 
