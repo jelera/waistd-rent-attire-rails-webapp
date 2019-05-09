2f50a31fec435b637161e66b06a61243d338ab21
Jose Elera <jelera@gmail.com>
2019-05-09 13:42:56 -0500

    add new route and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_09_151806) do
+ActiveRecord::Schema.define(version: 2019_05_09_152057) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "product_id"
